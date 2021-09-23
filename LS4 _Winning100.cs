7035266ddca241942a7b9f939dbb51a6ef946fc9
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2021-09-23 09:35:05 +0530

    snake and ladder added

---
+++ LS4 _Winning100.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladder_and_Snake_program
+{
+    class LS4__Winning100
+    {
+        public static void check()
+        {
+            int initial = 0;
+            int start = 0;
+
+            const int ladder = 1;
+            const int snake = 2;
+            
+            
+                while(initial<100)
+                {
+                    Random random = new Random();
+                    int diceroll = random.Next(1, 6);
+                    Console.WriteLine("dice ------------------>" + diceroll);
+
+
+                    switch (diceroll)
+                    {
+                        case ladder:
+                            initial = initial + diceroll;
+                            Console.WriteLine("Going UP ");
+
+                            break;
+
+                        case snake:
+                            initial = initial - diceroll;
+                            while (initial <= 0)
+                            {
+                                Console.WriteLine("back to start");
+                                initial = initial - initial;
+
+                                break;
+                            }
+
+                            break;
+
+                        default:
+                            initial = initial + diceroll;
+                            break;
+                    }
+
+                    initial = initial + start;
+
+                    Console.WriteLine("positin   " + initial);
+                }
+
+            }
+        }
+    }
+  
+
+        
+
+        
+    
+
+    
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladder_and_Snake_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        // LS1.LadderSnake();
+          // LS2_DICEAdded.DiceAdded();
+           //LS3_PlayerPlay.check();
+
+           LS4__Winning100.check();
+
+            Console.ReadKey();
+
+        }
+    }
+}
